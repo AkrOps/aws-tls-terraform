8c54f7e2e079e6572c4d1abbe20fa2f663e5caee
Joaquin Bermudez Castanheira <jbc@akrops.com>
2023-03-10 23:15:26 +0100

    chore: add second example with no ACM or Secrets Manager import

---
+++ examples/ex02-state-only/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "tls_certs" {
+  source = "../.."
+
+  project_id                = "tls-certs-ex01"
+  import_leaf_cert_into_acm = false
+  save_to_secrets_manager   = false
+
+  # Emulating a service running in a Kubernetes cluster
+  dns_sans = [
+    "my-svc",
+    "my-svc.my-ns",
+    "my-svc.my-ns.svc",
+    "my-svc.my-ns.svc.cluster.local",
+    "localhost"
+  ]
+
+  ip_sans = [
+    "127.0.0.1",
+    "10.0.0.1",
+    "10.0.0.2",
+    "10.0.0.3",
+  ]
+}
